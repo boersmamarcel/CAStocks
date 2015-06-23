2d7ff92e7ebf332751a0e2c3d56581de83c176e9
alexdegeus <alex_92@live.nl>
2015-06-23 18:46:11 +0200

    Update cluster
    now adds array of amount of +1 in cluster

--- Code/cluster_func.py
+++ Code/cluster_func.py
@@ -6,15 +6,29 @@ from scipy.ndimage import measurements
 #       - lw: field with cluster labels, inactive traders are labeled as 0
 #       - area: array with area size of each cluster, cluster label corresponds to area index
 #       - num: total number of clusters
+#       - cluster_ones: number of +1 in cluster
 
 def cluster(field):
     
-    field = np.absolute(field) # reduce field to active/inactive traders
-    field = field == 1 # get field of True/False values
-    lw, num = measurements.label(field) # lw: matrix with cluster numbers, num: total number of clusters 
-    area = measurements.sum(field, lw, index=np.arange(num + 1)) # get array of cluster size
+    field_abs = np.absolute(field) # reduce field to active/inactive traders
+    field_abs = field_abs == 1 # get field of True/False values
+    lw, num = measurements.label(field_abs) # lw: matrix with cluster numbers, num: total number of clusters 
+    area = measurements.sum(field_abs, lw, index=np.arange(1,num+1)) # area: matrix of cluster size
     
-    return lw, area, num
+    cluster_ones = np.zeros(num) # define empty array
+    for i in range(1,num+1): # loop clusters
+        cluster_ones[i-1] = (np.where(np.logical_and(lw==i,field==1))[0]).size # get numberof +1 in cluster
+       
+    return lw, area, num, cluster_ones
+ 
+## test  script  
+#f = np.random.choice([0,-1,1], 10*10, [0.8,0.1,0.1]).reshape(10,10)
+#a,b,c,d = cluster(f)
+#print f
+#print a
+#print b
+#print c
+#print d
     
 
     
